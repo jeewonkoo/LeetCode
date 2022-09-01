a3f6d83545ffb36525bbcca7c245eff376f54be4
Jeewon Koo <jeewonkoo@gmail.com>
2022-08-31 21:20:42 -0500

    417. Pacific Atlantic Water Flow

---
+++ python3/PacificAtlanticWaterFlow.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
+        pacific = [(0, i) for i in range(n)] + [(i, 0) for i in range(1, m)]
+        atlantic = [(m-1, i) for i in range(n)] + [(i, n-1) for i in range(m-1)]
+        
+        def bfs(queue):
+            visited = set()
+            dq = deque(queue)
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            while dq:
+                x, y = dq.popleft()
+                visited.add((x,y))
+                for direction in directions:
+                    new_x, new_y = direction[0]+x, direction[1]+y
+                    if -1 < new_x < m and -1 < new_y < n and (new_x, new_y) not in visited and heights[new_x][new_y] >= heights[x][y]:
+                        dq.append((new_x, new_y))
+            return visited
+        return bfs(pacific) & bfs(atlantic)
